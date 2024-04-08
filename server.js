6c0af6cc4a8c2ad26350d3aff497e4bfef68056a
florina8 <hateganflorina90@gmail.com>
2024-04-08 17:28:11 +0300

    Request Listener

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require("http");//anunt ca voi folosi 
+const http = require('http');//anunt ca voi folosi 
+const app = require("./app/app");
 //require pentru a importa
 require(`dotenv`).config();//trebuie sa aiba un fisier .env 
 
 
 
-http.createServer().listen(process.env.port, () => {
+http.createServer(app).listen(process.env.port, () => {
  //crearea serverului pe portul 3000
     console.log(`Server is running on port: ${process.env.port}`);
 
