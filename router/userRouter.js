818092c09740fe3169282be90d474999762751e2
florina8 <hateganflorina90@gmail.com>
2024-04-09 12:32:19 +0300

    Router

---
+++ router/userRouter.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+
+//http://localhost:3001/users
+router.get('/', (req, resp, next)=>{
+    resp.status(200).json({
+        message:'Successful - GET',
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+
+//GET
+//http://localhost:3001/users/34
+//the id can be anything 
+router.get("/:id",(req, res, next)=>{
+res.status(200).json({
+    message: 'Successful - GET by id',
+    metadata: {
+        id: req.params.id,
+        hostname: req.hostname,
+        method: req.method,
+    },
+   })
+  });
+  //http://localhost:3001/users
+router.post('/', (req, resp, next)=>{
+    const name = req.body.name
+    resp.status(201).json({
+        message:'Successful -POST',
+        metadata: {
+            name: name,
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+//PUT
+//http://localhost:3001/users/34
+//the id can be anything 
+router.put("/:id",(req, res, next)=>{
+    res.status(200).json({
+        message: 'Successful - PUT by id',
+        metadata: {
+            id: req.params.id,
+            hostname: req.hostname,
+            method: req.method,
+        },
+       })
+      });
+//DELETE
+//http://localhost:3001/users/34
+//the id can be anything 
+router.delete("/:id",(req, res, next)=>{
+    res.status(200).json({
+        message: 'Successful - DELETE by id',
+        metadata: {
+            id: req.params.id,
+            hostname: req.hostname,
+            method: req.method,
+        },
+       })
+      });
+module.exports = router;
