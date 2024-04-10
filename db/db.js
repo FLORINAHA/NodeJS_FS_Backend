592196cf10bc1105aa5c0611478676a48773df8e
florina8 <hateganflorina90@gmail.com>
2024-04-10 19:12:21 +0300

    Updated Router, New User Router, DB functionality

--- db/db.js
+++ db/db.js
@@ -1,10 +1,33 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const User = require('../models/userModel');
 
 
 const connect = async () => {
-    await mongoose.connect(process.env.mongo)
+    try {
+        await mongoose.connect(process.env.mongo);
         console.log('MongoDB is up and running');
-    };
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
 
-module.exports = { connect };
+const disconnect = async () => {
+    await mongoose.connection.close();
+};
+
+//obj {firstName: req.body.firstName, email: req.body.email}
+const findUser = async (obj) => {
+    User.findOne(obj);
+};
+
+const saveUser = async (newuser) => {
+    try {
+        return await newuser.save();
+    } catch (error) {
+        console.error('Error saving user:', error);
+        throw error;
+    }
+};
+
+module.exports = { connect, disconnect, findUser, saveUser };
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,75 +1,25 @@
-const express = require('express');
+const express = require ('express');
+
 const router = express.Router();
 
+router.post('/register',(req, res,next)=>{
+//findUser
+//if the user exist
+//return response taht says "email exist try loggin in"
+//else
+//encrypt the password
+//set the password with the encrypted password
+//save the user to the database
+router.post("/login", (req,res) => {
 
-//http://localhost:3001/users
-router.get('/', (req, resp, next)=>{
-    resp.status(200).json({
-      message:'Successful - GET',
-      metadata: {
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
+})
+
+
+
 });
-
-
-//GET
-//http://localhost:3001/users/34
-//the id can be anything 
-router.get("/:id",(req, res, next)=>{
-res.status(200).json({
-    message: 'Successful - GET by id',
-    metadata: {
-        id: req.params.id,
-        hostname: req.hostname,
-        method: req.method,
-    },
-   })
-  });
-
-
-  //http://localhost:3001/users
-router.post('/', (req, resp, next)=>{
-    const name = req.body.name
-    resp.status(201).json({
-      message:'Successful -POST',
-      metadata: {
-          name: name,
-          hostname: req.hostname,
-          method: req.method,
-        },
-    });
-});
-
-
-//PUT
-//http://localhost:3001/users/34
-//the id can be anything 
-router.put("/:id",(req, res, next)=>{
-    res.status(200).json({
-        message: 'Successful - PUT by id',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-       })
-      });
-
-
-//DELETE
-//http://localhost:3001/users/34
-//the id can be anything 
-router.delete("/:id",(req, res, next)=>{
-    res.status(200).json({
-        message: 'Successful - DELETE by id',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-       })
-      });
 
 module.exports = router;
+
+
+
+
